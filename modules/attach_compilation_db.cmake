e36b81d20ae310020d5f4461913aadd8bfe10a10
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-10-09 10:45:25 +0100

    handle compilation db for interface targets

--- modules/attach_compilation_db.cmake
+++ modules/attach_compilation_db.cmake
@@ -18,10 +18,21 @@ function(attach_compilation_db)
 
   set(file "compile_commands.json")
 
+  get_target_property(TRGT_TYPE ${acdb_TARGET} TYPE)
+  file(TO_CMAKE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/${file}" GENERATED_FILE)
+
+  if(${TRGT_TYPE} STREQUAL "INTERFACE_LIBRARY")
+    add_custom_command(OUTPUT ${GENERATED_FILE}
+      COMMAND ${CMAKE_COMMAND}
+      ARGS -E copy_if_different ${file} ${GENERATED_FILE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      VERBATIM)
+  else()
   add_custom_command(TARGET ${acdb_TARGET} POST_BUILD
     COMMAND ${CMAKE_COMMAND}
-    ARGS -E copy_if_different ${file} "${CMAKE_CURRENT_SOURCE_DIR}/${file}"
+        ARGS -E copy_if_different ${file} ${GENERATED_FILE}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     VERBATIM)
+  endif()
 endfunction()
 
