ae592d22fbecf358a53f4b44d852e4b8ce4e4810
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-10-09 10:28:25 +0100

    add modules

--- modules/cmake-utils.cmake
+++ modules/cmake-utils.cmake
@@ -8,7 +8,7 @@ cmake_minimum_required(VERSION 2.8.11)
 
 set(CMAKE_UTIL_VERSION_MAJOR "1")
 set(CMAKE_UTIL_VERSION_MINOR "1")
-set(CMAKE_UTIL_VERSION_PATCH "0")
+set(CMAKE_UTIL_VERSION_PATCH "1")
 
 string(CONCAT CMAKE_UTIL_VERSION
   ${CMAKE_UTIL_VERSION_MAJOR} "."
@@ -21,6 +21,9 @@ string(CONCAT CMAKE_UTIL_VERSION
 
 include(FindPythonModule)
 include(FindLLVMPolly)
+include(Findjemalloc)
+include(Findleveldb)
+include(attach_compilation_db)
 include(format_check)
 include(get_version)
 include(set_policies)
