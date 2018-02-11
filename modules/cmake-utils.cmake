dbc66054c27cce8acaa30ea2b191f2bd8e6c7450
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-02-11 17:02:00 +0100

    remove version from module package

--- modules/cmake-utils.cmake
+++ modules/cmake-utils.cmake
@@ -6,18 +6,7 @@
 
 cmake_minimum_required(VERSION 2.8.11)
 
-set(CMAKE_UTIL_VERSION_MAJOR "1")
-set(CMAKE_UTIL_VERSION_MINOR "1")
-set(CMAKE_UTIL_VERSION_PATCH "4")
-
-string(CONCAT CMAKE_UTIL_VERSION
-  ${CMAKE_UTIL_VERSION_MAJOR} "."
-  ${CMAKE_UTIL_VERSION_MINOR} "."
-  ${CMAKE_UTIL_VERSION_PATCH})
-
-
 #
-
 
 include(FindPythonModule)
 #include(FindLLVMPolly)
