b499dc4547e6c1d2e2c1046cd4d33e9337a2b604
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-03 17:24:06 +0100

    rename provided target

--- modules/AddCleanAllTarget.cmake
+++ modules/AddCleanAllTarget.cmake
@@ -3,6 +3,6 @@
 function(add_cleanall_target)
   add_custom_target(clean_all
     COMMAND "${CMAKE_COMMAND}" "--build" "${CMAKE_BINARY_DIR}"
-    "--target" "clean")
+    "--target" "cleanall")
 endfunction()
 
