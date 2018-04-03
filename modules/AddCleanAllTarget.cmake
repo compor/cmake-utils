707a3fe3f3d5906dd66e406b927e4c0f37056594
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-03 17:38:09 +0100

    restructrure and provide parameter support

--- modules/AddCleanAllTarget.cmake
+++ modules/AddCleanAllTarget.cmake
@@ -1,8 +1,29 @@
 # cmake file
 
-function(add_cleanall_target)
+include(CMakeParseArguments)
+
+function(add_clean_all_target)
+  set(options)
+  set(oneValueArgs TARGET)
+  set(multiValueArgs)
+
+  cmake_parse_arguments(ACAT "${options}" "${oneValueArgs}"
+    "${multiValueArgs}" ${ARGN})
+
+  if(NOT ACAT_TARGET)
+    set(ACAT_TARGET "cleanall")
+  endif()
+
+  if(TARGET ${ACAT_TARGET})
+    message(FATAL_ERROR "Target ${ACAT_TARGET} already exists")
+  endif()
+
+  if(ARGN)
+    message(FATAL_ERROR "extraneous arguments provided")
+  endif()
+
   add_custom_target(clean_all
     COMMAND "${CMAKE_COMMAND}" "--build" "${CMAKE_BINARY_DIR}"
-    "--target" "cleanall")
+    "--target" "${ACAT_TARGET}")
 endfunction()
 
